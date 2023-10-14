8b91661fc49880779c030965d8ee6f7174055553
Resul Acar <acar-resul-38@hotmail.com>
2023-10-14 16:16:06 +0300

    first commit

---
+++ src/resulAcar.java
@@ -0,0 +1,15 @@
+public class resulAcar {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Github");
+
+        /*
+        git init
+        git add .
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/racar38/resulAcar.git
+        git push -u origin main
+         */
+    }
+
+}
